0b31738d70fd6c2193c7b3dc7619af79cf8fb53c
jinheumkim <126999253+jinheumkim@users.noreply.github.com>
2023-09-14 17:14:52 +0900

    [Bronze IV] Title: 주사위 세개, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,14 @@
+A, B, C = input().split()
+A = int(A)
+B = int(B)
+C = int(C)
+if A == B == C:
+    print(10000+A*1000)
+elif A == B:
+    print(1000+A*100)
+elif A == C:
+    print(1000+A*100)
+elif B == C:
+    print(1000+B*100)
+else:
+    print((max(A,B,C))*100)
